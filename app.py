e13aeb8713dbb062b248e6876834c1026a443736
vinay kumar <156493268+vinaykumar095@users.noreply.github.com>
2025-05-21 18:10:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
+import os
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = secrets.token_hex(16)
